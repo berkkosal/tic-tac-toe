32515de0f687c37383569a9eabca90fce36d1ebf
Berk Koşal <kosalnberk@gmail.com>
2023-12-02 15:35:23 +0300

    Update GameBoard.jsx

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -9,18 +9,18 @@ const initialGameBoard = [
 
 export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
 
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-    // function handleSelectSquare(rowIndex, colIndex) {
-    //     setGameBoard((prevGameBoard) => {
-    //         const updatedBoard =[...prevGameBoard.map(innerArray => [...innerArray])];
-    //         //updatedBoard değişkenine deep copy yapıyoruz direkt olarak müdahale etmemek için.
-    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-    //         return updatedBoard;
-    //     });
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard =[...prevGameBoard.map(innerArray => [...innerArray])];
+            //updatedBoard değişkenine deep copy yapıyoruz direkt olarak müdahale etmemek için.
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+            return updatedBoard;
+        });
 
-    //     onSelectSquare();
-    // }
+        onSelectSquare();
+    }
 
     return (
         <ol id='game-board'>
